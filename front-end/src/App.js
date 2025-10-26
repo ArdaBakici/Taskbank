6531c822f0d94f292c70389a097223ea814482f7
PaudelSiddhanta <sp7007@nyu.edu>
2025-10-25 20:32:36 -0400

    Added Taskview page for viewing tasks

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TaskView from "./pages/TaskView";
+
+function App() {
+  return (
+    <div className="App">
+      <TaskView />
+    </div>
+  );
+}
+
+export default App;
