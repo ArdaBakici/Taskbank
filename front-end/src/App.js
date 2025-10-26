054ccbc1f6551dc4a40f8fc53c169e3d68d3c9f7
PaudelSiddhanta <sp7007@nyu.edu>
2025-10-26 19:32:37 -0400

    Implement ProjectView page with consistent dashboard styling

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import AllTasks from "./pages/AllTasks";
 import AllProjects from "./pages/AllProjects";
+import ProjectView from "./pages/ProjectView";
 import TaskView from "./pages/TaskView";
 import AddTask from "./pages/AddTask";
 import AddProject from "./pages/AddProject";
@@ -26,7 +27,10 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/tasks" element={<AllTasks />} />
         <Route path="/tasks/new" element={<AddTask />} />
+        
         <Route path="/task/:id" element={<TaskView />} />
+        <Route path="/project/:id" element={<ProjectView />} />
+
         <Route path="/tasks/edit/:id" element={<EditTask />} />
         <Route path="/projects" element={<AllProjects />} />
         <Route path="/projects/new" element={<AddProject />} />
@@ -39,3 +43,8 @@ function App() {
 }
 
 export default App;
+
+
+
+
+// {/* <Route path="/task/:id" element={<TaskView />} /> */}
