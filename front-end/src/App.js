6e4cf6587bdcf9e0c7fd7879a9a74cdeb8c52c87
sihyunlkim <sk11019@nyu.edu>
2025-10-26 13:39:24 -0400

    Uncomment commented code

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,10 +4,12 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import AllTasks from "./pages/AllTasks";
 import AllProjects from "./pages/AllProjects";
-//import TaskView from "./pages/TaskView";
+import TaskView from "./pages/TaskView";
 import AddTask from "./pages/AddTask";
 import AddProject from "./pages/AddProject";
 import EditTask from "./pages/EditTask";
+import EditProject from "./pages/EditProject";
+
 
 
 function App() {
@@ -18,9 +20,11 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/tasks" element={<AllTasks />} />
         <Route path="/tasks/new" element={<AddTask />} />
+        <Route path="/task/:id" element={<TaskView />} />
         <Route path="/tasks/edit/:id" element={<EditTask />} />
         <Route path="/projects" element={<AllProjects />} />
         <Route path="/projects/new" element={<AddProject />} />
+        <Route path="/projects/edit/:id" element={<EditProject />} />
         <Route path="*" element={<Login />} /> {/* default fallback */}
       </Routes>
     </Router>
